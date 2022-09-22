3262c677e687b0235519b8f69f409b9e49dd11f2
Faiz <88323746+ShaikFaizanAhmad@users.noreply.github.com>
2022-09-22 16:52:47 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $e-mail = $_POST['e-mail'];
+    $phone = $_POST['number'];
+    $dateofbirth = $_POST['date'];
+    $password = $_POST['password'];
+    
+?>
